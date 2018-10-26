e9835654d7148616fc5b7a40d1b6932b65127cfd
GosuUnDeadGhost <levkovich2806@gmail.com>
2018-10-26 16:55:40 +0300

    office

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import Tracker from './tracker/tracker.js';
+import Home from './home/home.js';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit1 <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>Tracker Check</h1>
+          <ul className="header">
+            <li><NavLink to="/">Главная</NavLink></li>
+            <li><NavLink to="/tracker">Трекер</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/tracker" component={Tracker}/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
